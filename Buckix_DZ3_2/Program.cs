c76d86ead1fa0e0ca65b136b03b886a9c9ccaa05
Varren999 <varren999@gmail.com>
2024-12-07 08:32:49 +0200

    work version

---
+++ Buckix_DZ3_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//========================================================================
+// Создайте класс «Веб-сайт». Необходимо хранить в полях класса:
+// название сайта, путь к сайту, описание сайта, ip адрес сайта.
+// Реализуйте методы класса для ввода данных, вывода данных, реализуйте
+// доступ к отдельным полям через методы класса. 
+//========================================================================
+using System;
+
+namespace Buckix_DZ3_2
+{
+    class Program
+    {
+        static int Main()
+        {
+            Console.Title = "Создайте класс «Веб-сайт»";
+            string[] msg = { "Введите название сайта: ", "Введите путь к сайту: ", "Введите описание сайта: ", "Введите IP адрес сайта: " };
+            Console.WriteLine();
+            Website website = new Website { Name = InputText(msg[0]), Path = InputText(msg[1]), Note = InputText(msg[2]), Ip = InputText(msg[3]) };
+            Console.WriteLine(website.ToString());
+            return 0;
+        }
+
+        static string InputText(string msg)
+        {
+            Console.Write(msg);
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ Buckix_DZ3_2/Website.cs
@@ -0,0 +1,19 @@
+﻿namespace Buckix_DZ3_2
+{
+    internal class Website
+    {
+        private string name;
+        public string Name { get => name; set { name = value; } }
+
+        private string path;
+        public string Path { get => path; set { path = value; } }
+
+        string note;
+        public string Note { get => note; set { note = value; } }
+
+        string ip;
+        public string Ip { get => ip; set { ip = value; } }
+
+        public override string ToString() => $"Название сайта: {Name}\nАдрес сайта: {Path}\nОписание: {Note}\nIP: {Ip}";
+    }
+}
